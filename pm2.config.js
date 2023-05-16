2d0edde932f0d560d1d680a5581c04f2beb540fb
Ubuntu <ubuntu@ip-172-31-22-230.ap-southeast-1.compute.internal>
2023-05-16 03:17:43 +0000

    add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: 'npm run start'
+    },
+  ],
+};
